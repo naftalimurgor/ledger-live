406b5e443c13f7978f226f0a175cfebf9f4a8b34
0xkvn <44363395+lambertkevin@users.noreply.github.com>
2024-05-16 23:12:45 +0200

    Apply improved types & architecture to Polkadot

--- libs/coin-modules/coin-polkadot/src/bridge/broadcast.test.ts
+++ libs/coin-modules/coin-polkadot/src/bridge/broadcast.test.ts
@@ -13,7 +13,11 @@ describe("broadcast", () => {
   it("calls explorer for broadcast operation", async () => {
     // WHEN
     await broadcast({
-      signedOperation: { signature: "SIGNATURE", operation: createFixtureOperation() },
+      account: {} as any,
+      signedOperation: {
+        signature: "SIGNATURE",
+        operation: createFixtureOperation(),
+      },
     });
 
     // THEN
@@ -26,7 +30,10 @@ describe("broadcast", () => {
     const operation = createFixtureOperation();
 
     // WHEN
-    const result = await broadcast({ signedOperation: { signature: "SIGNATURE", operation } });
+    const result = await broadcast({
+      account: {} as any,
+      signedOperation: { signature: "SIGNATURE", operation },
+    });
 
     // THEN
     expect(result).not.toEqual(operation);
