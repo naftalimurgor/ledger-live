1b2e95926220e27e9e260d51bb37764e0d49cb4b
0xkvn <44363395+lambertkevin@users.noreply.github.com>
2024-05-16 23:15:01 +0200

    Apply improved types & architecture to Stellar

--- libs/ledger-live-common/src/families/stellar/bridge/js.ts
+++ libs/ledger-live-common/src/families/stellar/bridge/js.ts
@@ -1,32 +1,22 @@
-import { makeAccountBridgeReceive } from "../../../bridge/jsHelpers";
-import { defaultUpdateTransaction } from "@ledgerhq/coin-framework/bridge/jsHelpers";
 import type { AccountBridge, CurrencyBridge } from "@ledgerhq/types-live";
+import { defaultUpdateTransaction } from "@ledgerhq/coin-framework/bridge/jsHelpers";
+import { makeAccountBridgeReceive } from "../../../bridge/jsHelpers";
+import estimateMaxSpendable from "../estimateMaxSpendable";
+import getTransactionStatus from "../getTransactionStatus";
+import { scanAccounts, sync } from "../synchronization";
+import prepareTransaction from "../prepareTransaction";
+import createTransaction from "../createTransaction";
+import signOperation from "../signOperation";
 import type { Transaction } from "../types";
-import { scanAccounts, sync } from "../js-synchronization";
-import getTransactionStatus from "../js-getTransactionStatus";
-import estimateMaxSpendable from "../js-estimateMaxSpendable";
-import createTransaction from "../js-createTransaction";
-import prepareTransaction from "../js-prepareTransaction";
-import signOperation from "../js-signOperation";
-import broadcast from "../js-broadcast";
-import type { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
-
-const preload = async (): Promise<any> => {};
-
-const hydrate = (): void => {};
-
-const receive = makeAccountBridgeReceive();
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getPreloadStrategy = (currency: CryptoCurrency): any => ({});
+import broadcast from "../broadcast";
 
 const currencyBridge: CurrencyBridge = {
-  getPreloadStrategy,
-  preload,
-  hydrate,
+  preload: async () => {},
+  hydrate: () => {},
   scanAccounts,
 };
 
+const receive = makeAccountBridgeReceive();
 const accountBridge: AccountBridge<Transaction> = {
   createTransaction,
   updateTransaction: defaultUpdateTransaction,
--- libs/ledger-live-common/src/families/stellar/estimateMaxSpendable.ts
+++ libs/ledger-live-common/src/families/stellar/estimateMaxSpendable.ts
@@ -1,25 +1,21 @@
 import { getMainAccount } from "../../account";
-import createTransaction from "./js-createTransaction";
-import prepareTransaction from "./js-prepareTransaction";
-import getTransactionStatus from "./js-getTransactionStatus";
-import type { Account, AccountLike } from "@ledgerhq/types-live";
+import createTransaction from "./createTransaction";
+import prepareTransaction from "./prepareTransaction";
+import getTransactionStatus from "./getTransactionStatus";
+import type { AccountBridge } from "@ledgerhq/types-live";
 import type { Transaction } from "./types";
 import BigNumber from "bignumber.js";
 
 const notCreatedStellarMockAddress = "GAW46JE3SHIAYLNNNQCAZFQ437WB5ZH7LDRDWR5LVDWHCTHCKYB6RCCH";
 
-const estimateMaxSpendable = async ({
+const estimateMaxSpendable: AccountBridge<Transaction>["estimateMaxSpendable"] = async ({
   account,
   parentAccount,
   transaction,
-}: {
-  account: AccountLike;
-  parentAccount: Account | null | undefined;
-  transaction: Transaction | null | undefined;
-}): Promise<BigNumber> => {
+}) => {
   const mainAccount = getMainAccount(account, parentAccount);
   const t = await prepareTransaction(mainAccount, {
-    ...createTransaction(),
+    ...createTransaction(account),
     ...transaction,
     recipient: transaction?.recipient || notCreatedStellarMockAddress,
     // not used address
